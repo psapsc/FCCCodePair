8dedad5307ce2692c8ce505da0839f8e072d1d17
Wesley McCann <septimus98@gmail.com>
2015-07-24 13:52:41 -0400

    Updated CSS and Javascript css control

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 function formControl (id){
   element = document.getElementById(id);
-  if(id === "signup"){
+  if(id === "signup" && !element.classList.contains('clicked')){
     document.getElementById('signin').style.display = "none";
     element.classList.add("clicked");
     document.getElementById('signon').style.marginTop = "-145px";
     document.getElementById('signupForm').style.display = 'inline';
   }
-  if(id === "signup" && element.classList.contains('clicked')){
-    document.getElementById('signin').style.display = "none";
+  else if(id === "signup" && element.classList.contains('clicked')){
+    document.getElementById('signin').style.display = "inline";
     element.classList.remove('clicked');
+    document.getElementById('signon').style.marginTop = "-100px";
+    document.getElementById('signupForm').style.display = 'none';
+  }
+
+  if(id === "signin" && !element.classList.contains('clicked')){
+    document.getElementById('signup').style.display = "none";
+    element.classList.add("clicked");
+    document.getElementById('signon').style.marginTop = "-145px";
+    document.getElementById('signinForm').style.display = 'inline';
+  }
+  else if(id === "signin" && element.classList.contains('clicked')){
+    document.getElementById('signup').style.display = "inline";
+    element.classList.remove('clicked');
+    document.getElementById('signon').style.marginTop = "-100px";
+    document.getElementById('signinForm').style.display = 'none';
   }
 }
