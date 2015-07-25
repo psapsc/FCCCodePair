d8a024ff4502ab36c4be0e570645b81149ef8235
Wesley McCann <septimus98@gmail.com>
2015-07-25 09:25:32 -0400

    Updating user database insertion

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,17 @@
 var signup = function(username, email, password){
-  
+  MongoClient.connect(url);
+  db.collection('users').insertOne( {
+    "userInfo": {
+      "uid" : 1,
+      "username" : "septimus",
+      "firstName" : "Wesley",
+      "lastName" : "McCann",
+      "email" : "septimus98@gmail.com",
+      "password" : "testpass"
+    },
+  }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the users collection.");
+    callback(result);
+  });
 }
